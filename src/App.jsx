979e98bd5cb2c9b617a7da4c28589792fa54bdb5
siddharth03-del <siddharthsingh9361@gmail.com>
2024-12-01 18:37:49 +0530

    Added vercel speed insight

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import { useState } from "react";
 import { MyContext } from "./context";
 import Routing from "./Routes.jsx";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 function App(){
   return (
           <MyContext.Provider>
-
+          <SpeedInsights/>
           <Routing/>
 
           </MyContext.Provider>
