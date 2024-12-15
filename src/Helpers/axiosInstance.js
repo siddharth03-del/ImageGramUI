315872e907b085ecbf54a9572899b457791d4aca
siddharth03-del <siddharthsingh9361@gmail.com>
2024-12-15 11:10:32 +0530

    fixed signin

--- src/Helpers/axiosInstance.js
+++ src/Helpers/axiosInstance.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 import { localUrl, renderUrl } from "./storeUrl";
+import { red } from "@mui/material/colors";
 const axios_instance = axios.create({
-    baseURL : localUrl,
+    baseURL : renderUrl,
 })
 export default axios_instance;
